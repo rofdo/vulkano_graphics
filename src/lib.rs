03218bc156940d944d25ced0475c4f773eb3e5dd
rofdo <git.hub@ewe.net>
2024-12-23 16:08:40 +0100

    physical device

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
-use vulkano::{instance::{Instance, InstanceCreateFlags, InstanceCreateInfo}, VulkanLibrary};
 use std::{error::Error, sync::Arc};
+use vulkano::{
+    instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
+    VulkanLibrary,
+};
 
 fn get_instance() -> Result<Arc<Instance>, Box<dyn Error>> {
     let library = VulkanLibrary::new()?;
     let instance = Instance::new(
         library,
-        InstanceCreateInfo{
+        InstanceCreateInfo {
             flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
             ..Default::default()
         },
-    )
-    ?;
+    )?;
     Ok(instance)
 }
 
-pub fn create_window() -> Result<(),Box<dyn Error>> {
+pub fn create_window() -> Result<(), Box<dyn Error>> {
     let instance = get_instance()?;
+    let mut physical_devices = instance.enumerate_physical_devices()?;
+    let physical_device = physical_devices.next().ok_or("No physical devices")?;
     Ok(())
 }
+
